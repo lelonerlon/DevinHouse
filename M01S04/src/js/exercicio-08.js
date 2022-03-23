c90af07f3f38400628a038386bea50692c7535e9
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-03-23 09:47:26 -0300

    Exercicio 08

---
+++ M01S04/src/js/exercicio-08.js
@@ -0,0 +1,29 @@
+let cidades = [
+    { nome: 'Patos de Minas', populacao: 153585 },
+    { nome: 'Lages', populacao: 157349 },
+    { nome: 'Ibiúna', populacao: 79479 },
+    { nome: 'Maringá', populacao: 403063 },
+    { nome: 'Curitiba', populacao: 1963726 },
+    { nome: 'Florianópolis', populacao: 508826 },
+    { nome: 'Pato Branco', populacao: 84779 },
+],
+    cidadesGrandes = [];
+
+cidadesGrandes = cidades.filter(function (cidade) {
+     return cidade.populacao > 200000;
+})
+
+function compare(a,b) {
+    if (a.populacao < b.populacao)
+       return -1;
+    if (a.populacao > b.populacao)
+      return 1;
+    return 0;
+
+  }
+
+  cidadesGrandes.sort(compare);
+
+console.log(cidadesGrandes)
+
+
