072a2deb97132cfc52a3ae7fa49cb49ca2e77e45
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-04-09 17:58:00 -0300

    Exercicio 08

--- M01S06/src/js/buscar-cep.js
+++ M01S06/src/js/buscar-cep.js
@@ -4,55 +4,62 @@ export const getAdress = async () => {
   const dataStreet = document.getElementById('logradouro').value;
   const data = await fetchApiAll(dataState, dataCity, dataStreet);
   console.log(data);
-  // if (data) {
-  //   const adressList = document.getElementById('resultado');
-  //   adressList.textContent = `
-  //   Logradouro: ${data.logradouro}
-  //   Bairro: ${data.bairro}
-  //   Complemento: ${data.complemento}
-  //   Cep: ${data.cep}
-  //   Ddd: ${data.ddd}
-  //   Ibge: ${data.ibge}
-  //   Cidade: ${data.localidade}
-  //   UF: ${data.uf}
-  //   `;
-  //   }
+  let adressList = document.getElementById('resultado');
+  if (data) {
+    data.forEach((dado) => {
+      const el = document.createElement('p')
+      el.textContent = `
+      Logradouro: ${dado.logradouro}
+      Bairro: ${dado.bairro}
+      Complemento: ${dado.complemento}
+      Cep: ${dado.cep}
+      Ddd: ${dado.ddd}
+      Ibge: ${dado.ibge}
+      Cidade: ${dado.localidade}
+      UF: ${dado.uf}
+      `;
+      adressList.appendChild(el);
+    });
+  } if (data.length === 0) {
+    adressList.innerHTML = ('Nenhum registro foi encontrado');
+  }
+
 };
 
 
-  export async function fetchAPI(cep) {
-    const base_url = 'https://viacep.com.br'
-    const url = `${base_url}/ws/${cep}/json/`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      return await response.json();
-    }
-    return alert('No momento estamos sem comunicação com a API');
-
+export async function fetchAPI(cep) {
+  const base_url = 'https://viacep.com.br'
+  const url = `${base_url}/ws/${cep}/json/`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    return await response.json();
   }
+  return alert('No momento estamos sem comunicação com a API');
 
-  export async function fetchApiAll(state, city, street) {
-    const base_url = 'https://viacep.com.br'
-    const url = `${base_url}/ws/${state}/${city}/${street}/json/`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      return await response.json();
-    }
-    return alert('No momento estamos sem comunicação com a API');
+}
 
+export async function fetchApiAll(state, city, street) {
+  const base_url = 'https://viacep.com.br'
+  const url = `${base_url}/ws/${state}/${city}/${street}/json/`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    return await response.json();
   }
+  return alert('No momento estamos sem comunicação com a API');
+
+}
 
 
 
