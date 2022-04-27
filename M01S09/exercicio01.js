8811049674952e60f7c80c471c03833eb98b8dfb
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-04-26 22:11:29 -0300

    Exercicio 01

---
+++ M01S09/exercicio01.js
@@ -0,0 +1,4 @@
+var idade = 33;
+var nome = 'Erlon';
+var pessoa = "Ol\u00E1 meu nome \u00E9 ".concat(nome, ", e tenho ").concat(idade, " anos");
+console.log(pessoa);
---
+++ M01S09/exercicio01.ts
@@ -0,0 +1,5 @@
+let idade: number = 33;
+const nome: string = 'Erlon';
+var pessoa: string = `Olá meu nome é ${nome}, e tenho ${idade} anos`;
+
+console.log(pessoa)
