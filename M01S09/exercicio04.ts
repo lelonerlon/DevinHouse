b05655fff6129c262872c3a745f830e99ed0d268
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-04-29 20:24:22 -0300

    exercicio04

---
+++ M01S09/exercicio04.ts
@@ -0,0 +1,11 @@
+class Filme {
+    nome: string;
+    anoDeLancamento: number;
+    diretor: string;
+}
+
+
+let filme = new Filme();
+filme.nome = "A volta dos que não foram";
+filme.anoDeLancamento = 1990;
+filme.diretor = 'Billy The Kid';
