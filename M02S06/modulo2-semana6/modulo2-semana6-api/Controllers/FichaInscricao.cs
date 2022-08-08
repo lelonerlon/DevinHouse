69d82e8ddd2a5c94d2671639442101833d7101dc
Erlon Ressurreição <erlon.ressurreicao@gmail.com>
2022-08-08 15:48:07 -0300

    ex04

---
+++ M02S06/modulo2-semana6/modulo2-semana6-api/Controllers/FichaInscricao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class FichaInscricao
+{
+	public FichaInscricao()
+	{
+
+	}
+}
+
+Nome
+Curso,
+Escolaridade
+Data de Nascimento
+Valor Curso
+Valor de Desconto
+Valor Multa
---
+++ M02S10/ApiMusicas/ApiMusicas/Models/Artista.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiMusicas.Models
+{
+    public class Artista
+    {
+        public string Nome { get; set; }
+        public string NomeArtistico { get; set; }
+        public string FotoUrl { get; set; }
+        
+        public string PaisOrigem { get; set; }
+    }
+}
